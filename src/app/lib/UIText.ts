599c710503e5fbb1453e5d51f76191dbe2216530
EfliukovMI <efliukovmi@tretyakov.ru>
2025-08-06 17:43:18 +0300

    feat: add new translations

---
+++ src/app/lib/UIText.ts
@@ -0,0 +1,62 @@
+export const UIText = {
+  about: {
+    ru: "Обо мне",
+    en: "About"
+  },
+  workExperience: {
+    ru: "Опыт работы",
+    en: "Work Experience"
+  },
+  education: {
+    ru: "Образование",
+    en: "Education"
+  },
+  skills: {
+    ru: "Навыки",
+    en: "Skills"
+  },
+  projects: {
+    ru: "Проекты",
+    en: "Projects"
+  },
+  workTime: {
+    ru: "Настоящее",
+    en: "Present"
+  },
+  print: {
+    ru: "Печать",
+    en: "Print"
+  },
+  translate: {
+    ru: "Translate to English",
+    en: "Перевести на Русский"
+  },
+  save: {
+    ru: "Сохранить",
+    en: "Save"
+  },
+  noChanges: {
+    ru: "Нет изменений",
+    en: "No changes"
+  },
+  saved: {
+    ru: "Сохранено",
+    en: "Saved"
+  },
+  error: {
+    ru: "Произошла ошибка",
+    en: "Error"
+  },
+  home: {
+    ru: "Главная страница",
+    en: "Home page"
+  },
+  signIn: {
+    ru: "Войти",
+    en: "Sign in"
+  },
+  edit:{
+    ru: "Редактировать",
+    en: "Edit"
+  }
+}

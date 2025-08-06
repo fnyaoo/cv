de40bf4c1a018155c52707cb359308e3e010ce80
EfliukovMI <efliukovmi@tretyakov.ru>
2025-08-06 17:36:13 +0300

    feat: add Sonner for toast

---
+++ src/app/providers/ToastProvider.tsx
@@ -0,0 +1,16 @@
+// @/_providers/ToastProvider.tsx
+'use client'
+
+import { Toaster } from 'sonner'
+
+export function ToastProvider({ children }: { children: React.ReactNode }) {
+  
+  return (
+    <>
+      {children}
+      <Toaster
+        position="top-center"
+      />
+    </>
+  )
+}

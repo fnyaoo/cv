7132320a229b01104b58d8cb4834fd4566e517b9
EfliukovMI <efliukovmi@tretyakov.ru>
2025-08-07 17:10:53 +0300

    feat: add check auth for api

--- src/app/api/save-resume/route.ts
+++ src/app/api/save-resume/route.ts
@@ -1,11 +1,19 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import fs from 'fs'
 import path from 'path'
+import { getToken } from 'next-auth/jwt'
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
+
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+	if (!token) {
+		return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+	}
+  
   try {
     const data = await req.json()
-    const filePath = path.join(process.cwd(), '/src/data', 'resume-data.json')
+    const filePath = path.join(process.cwd(), '/data', 'resume-data.json')
 
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8')
 
